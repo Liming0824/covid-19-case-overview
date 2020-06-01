3d527bf6923bf2cc20d5613c10b291f85630742d
Liming Kang <lkang9208@yahoo.com>
2020-05-31 20:00:31 -0400

    Updates

---
+++ precache-manifest.0eb324b443dae5e48dc78de7416cc654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ad185a75424a6c3a2687bc8ba53f16",
+    "url": "/covid-19-case-overview/index.html"
+  },
+  {
+    "revision": "2e9de2a58cde0b0c74c9",
+    "url": "/covid-19-case-overview/static/css/main.c238edcb.chunk.css"
+  },
+  {
+    "revision": "3af1457c0c7d518001f2",
+    "url": "/covid-19-case-overview/static/js/2.c4c2954c.chunk.js"
+  },
+  {
+    "revision": "3df497401d324ee6ed76d4d98715bc5f",
+    "url": "/covid-19-case-overview/static/js/2.c4c2954c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9de2a58cde0b0c74c9",
+    "url": "/covid-19-case-overview/static/js/main.05f58f7c.chunk.js"
+  },
+  {
+    "revision": "ad3b59f67e1b068536eb",
+    "url": "/covid-19-case-overview/static/js/runtime-main.d77faeff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-case-overview/static/media/logo.5d5d9eef.svg"
+  }
+]);
